195bb5a2b85c6f63b7db53c495c362a0cff4db7e
LyEEhD <ldh041203@gmail.com>
2025-09-27 14:23:15 +0900

    fix: apply gitignore

--- MnA_BE/DailyInsight/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DailyinsightConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "DailyInsight"
--- MnA_BE/DailyInsight/sample1/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import JsonResponse
-import json
-
-def hello(request):
-    return JsonResponse({"message": "Hello, world!"})
