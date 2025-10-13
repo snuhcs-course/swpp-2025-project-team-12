47cb6029f2aa66939fcfa8ead087ecb9f738f7f3
simjaemin1 <simjaemin1@snu.ac.kr>
2025-10-13 11:26:31 +0900

    resolved conflict

--- MnA_BE/apps/user/models.py
+++ MnA_BE/apps/user/models.py
@@ -3,16 +3,15 @@ from django.db import models
 TINY_TEXT = 255
 
 class User(models.Model):
-    # dev 쪽 유지: AutoField (이미 배포/마이그레이션과 충돌 최소)
-    id = models.AutoField(primary_key=True)
-
+    id = models.CharField(primary_key=True, editable=False, max_length=36)
     password = models.CharField(max_length=TINY_TEXT)
-    # 이름은 기본값 허용(HEAD에서 default 있었음)
     name = models.CharField(max_length=TINY_TEXT, default="")
+    refresh_token = models.CharField(max_length=TINY_TEXT, default="")
 
-    # HEAD에서 가져옴: refresh_token (선택 필드로)
-    refresh_token = models.CharField(max_length=TINY_TEXT, default="", blank=True)
+class Interests(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    interests = models.JSONField()
+    create_at = models.DateTimeField(auto_now_add=True)
 
-    # dev 쪽의 interests를 유지하되, 경고 해결: default=dict() → default=list
-    # 리스트 태그 저장 형태 권장: ["AI","2차전지", ...]
-    interests = models.JSONField(default=list)
+    class Meta:
+        ordering = ["-create_at"]
