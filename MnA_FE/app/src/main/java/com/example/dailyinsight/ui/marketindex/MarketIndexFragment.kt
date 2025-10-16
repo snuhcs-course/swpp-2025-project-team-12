aa44a90d90ac8ec24d987e9b93ae1db5642ba9ee
Kim Chanwoo <chanu@snu.ac.kr>
2025-10-16 16:16:55 +0900

    Notification -> Market Index로 refactor

--- MnA_FE/app/src/main/java/com/example/dailyinsight/ui/marketindex/MarketIndexFragment.kt
+++ MnA_FE/app/src/main/java/com/example/dailyinsight/ui/marketindex/MarketIndexFragment.kt
@@ -1,4 +1,4 @@
-package com.example.dailyinsight.ui.notifications
+package com.example.dailyinsight.ui.marketindex
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.dailyinsight.databinding.FragmentNotificationsBinding
+import com.example.dailyinsight.databinding.FragmentMarketIndexBinding
 
-class NotificationsFragment : Fragment() {
+class MarketIndexFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentMarketIndexBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val marketIndexViewModel =
+            ViewModelProvider(this).get(MarketIndexViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentMarketIndexBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        marketIndexViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- MnA_FE/app/src/main/java/com/example/dailyinsight/ui/marketindex/MarketIndexViewModel.kt
+++ MnA_FE/app/src/main/java/com/example/dailyinsight/ui/marketindex/MarketIndexViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.dailyinsight.ui.notifications
+package com.example.dailyinsight.ui.marketindex
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class MarketIndexViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
