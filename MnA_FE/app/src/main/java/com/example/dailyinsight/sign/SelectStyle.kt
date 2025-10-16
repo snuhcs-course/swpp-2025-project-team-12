2317c6b43d1f5425248c0f87512a0499313c7393
falsitas <leelee0325@snu.ac.kr>
2025-10-16 18:14:44 +0900

    refactor project directory

--- MnA_FE/app/src/main/java/com/example/dailyinsight/sign/SelectStyle.kt
+++ MnA_FE/app/src/main/java/com/example/dailyinsight/sign/SelectStyle.kt
@@ -1,16 +1,17 @@
-package com.example.dailyinsight
+package com.example.dailyinsight.sign
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.dailyinsight.R
 
-class signUp : AppCompatActivity() {
+class SelectStyle : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
+        setContentView(R.layout.activity_select_style)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
