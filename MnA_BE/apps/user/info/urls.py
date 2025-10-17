44ea03cc3a8358ce4f559f5e16a709e515b0e6dd
LyEEhD <ldh041203@gmail.com>
2025-10-18 00:30:18 +0900

    feat: endpoint for changing password

--- MnA_BE/apps/user/info/urls.py
+++ MnA_BE/apps/user/info/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from .password import password_view
 from .profile import profile_view
 from .name import name_view
 
 urlpatterns = [
+    path('password', password_view, name='password'),
     path('profile', profile_view, name='profile'),
     path('name', name_view, name='name'),
 ]
--- MnA_BE/apps/user/style/urls.py
+++ MnA_BE/apps/user/style/urls.py
@@ -3,6 +3,6 @@ from .root import style_view
 from .page import get_page
 
 urlpatterns = [
-    path('', style_view, name='style_view'),
-    path('page/<int:page_index>', get_page, name='get_page'),
+    path('', style_view, name='style'),
+    path('page/<int:page_index>', get_page, name='page'),
 ]
