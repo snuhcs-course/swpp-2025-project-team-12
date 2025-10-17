2f6b469e42d4362d0976bda9cf874ac338824fc7
LyEEhD <ldh041203@gmail.com>
2025-10-18 00:25:22 +0900

    fix: rollback require_auth, fix profile with str(user.id), blocked user name POST

--- MnA_BE/apps/user/info/name.py
+++ MnA_BE/apps/user/info/name.py
@@ -8,7 +8,7 @@ import json
 def name_view(request, user):
     """
     GET: get user's name. (check user by cookie)
-    POST: change user's name.
+    ["BLOCKED"] POST: change user's name.
     """
 
     if request.method == "GET":
@@ -17,25 +17,28 @@ def name_view(request, user):
         return JsonResponse({ "name": user.name }, status=200)
 
 
-    elif request.method == "POST":
-        ### POST ###
-
-        body = json.loads(request.body.decode('utf-8'))
-        name = body.get("name")
-
-        if name is None:
-            return JsonResponse({ "message": "NAME IS REQUIRED" }, status=400)
-
-        if not validate_name(name):
-            return JsonResponse({ "message": "INVALID NAME" }, status=400)
-
-        try:
-            user.name = name
-            user.save()
-        except Exception as e:
-            return JsonResponse({ "message": "NAME SAVE FAILED" }, status=500)
-
-        return JsonResponse({ "message": "NAME UPDATE SUCCESS" }, status=200)
+#       ### DON'T erase comment below!!! (just BLOCKED) ###
+#
+#
+#     elif request.method == "POST":
+#         ### POST ###
+#
+#         body = json.loads(request.body.decode('utf-8'))
+#         name = body.get("name")
+#
+#         if name is None:
+#             return JsonResponse({ "message": "NAME IS REQUIRED" }, status=400)
+#
+#         if not validate_name(name):
+#             return JsonResponse({ "message": "INVALID NAME" }, status=400)
+#
+#         try:
+#             user.name = name
+#             user.save()
+#         except Exception as e:
+#             return JsonResponse({ "message": "NAME SAVE FAILED" }, status=500)
+#
+#         return JsonResponse({ "message": "NAME UPDATE SUCCESS" }, status=200)
 
     else:
         return JsonResponse({ "message": "NOT ALLOWED METHOD" }, status=405)
