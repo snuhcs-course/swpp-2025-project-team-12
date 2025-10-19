328454bd7b0d726ad16e854db6ded4fb4d5c5b08
Kim Chanwoo <chanu@snu.ac.kr>
2025-10-19 21:52:41 +0900

    Refactor Appbar

--- MnA_FE/app/src/main/java/com/example/dailyinsight/MainActivity.kt
+++ MnA_FE/app/src/main/java/com/example/dailyinsight/MainActivity.kt
@@ -23,16 +23,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ServiceLocator.init(applicationContext)
-        enableEdgeToEdge()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.container)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         // Find the NavController
         // First, Get the NavHostFragment from the FragmentManager
