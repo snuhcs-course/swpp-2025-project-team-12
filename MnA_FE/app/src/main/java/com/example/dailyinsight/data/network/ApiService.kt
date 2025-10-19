d400d977ec221a665289f30aeae35ff6bcc2fc37
simjaemin1 <simjaemin1@snu.ac.kr>
2025-10-19 20:09:41 +0900

    fix(api): ApiService.kt

--- MnA_FE/app/src/main/java/com/example/dailyinsight/data/network/ApiService.kt
+++ MnA_FE/app/src/main/java/com/example/dailyinsight/data/network/ApiService.kt
@@ -26,7 +26,7 @@ data class SignUpResponse(
 )
 
 interface ApiService {
-    @GET("marketindex/stockindex/latest/")
+    @GET("marketindex/stockindex/latest")
     suspend fun getStockIndex(): ApiResponse
 
     @POST("apps/user/login")
