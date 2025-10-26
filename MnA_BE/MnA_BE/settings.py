5a4fb27c2e3007f5eefcc7b35efca0031c617ac6
LyEEhD <ldh041203@gmail.com>
2025-10-27 00:07:56 +0900

    fix: remove redundant folders, some bugs (print key)

--- MnA_BE/MnA_BE/settings.py
+++ MnA_BE/MnA_BE/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     # custom apps
     "corsheaders",
     "apps.user",
-    "apps.DailyInsight",
     "apps.MarketIndex",
     "apps.api",
 ]
@@ -55,7 +54,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+#    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
