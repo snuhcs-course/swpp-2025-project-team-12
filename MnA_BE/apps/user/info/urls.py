b6047dfcaf62479613f94e191f6a0822964f66bd
LyEEhD <ldh041203@gmail.com>
2025-11-05 20:14:08 +0900

    feat: add JSONField(Portfolio) and user/info/portfolio endpoints

--- MnA_BE/apps/user/info/urls.py
+++ MnA_BE/apps/user/info/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .password import PasswordView
 from .profile import ProfileView
+from .portfolio import PortfolioView
 from .name import NameView
 
 urlpatterns = [
@@ -13,6 +14,11 @@ urlpatterns = [
         'post': 'post',
         'delete': 'delete'
     }), name='profile'),
+
+    path('portfolio', PortfolioView.as_view({
+        'get': 'get',
+        'post': 'post'
+    }), name='portfolio'),
 
     path('name', NameView.as_view({
         'get': 'get',
